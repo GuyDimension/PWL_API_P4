0e3288ce6b625dd3b0ec36c6ec76862ae58bc363
GuyDimension <billycelino67@gmail.com>
2025-04-04 08:46:46 +0700

    Memasukkan file

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require ("express");
+const bodyParser = require ("body-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+let products = [
+    { id: 1, name: "Kopi Tubruk", price: 10000 },
+    { id: 2, name: "Teh Manis", price: 8000 }
+];
+/*router*/
+app.get("/products", (req, res) => {
+    res.json({
+        status: "success",
+        data: products
+    });
+});
+
+app.post("/products", (req, res) => {
+    const { name, price } = req.body;
+
+    if (!name || price === undefined) {
+        return res.status(400).json({
+            status: "error",
+            message: "Field 'name' dan 'price' wajib diisi"
+        });
+    }
+
+    const newProduct = {
+        id: products.length + 1,
+        name,
+        price
+    };
+
+    products.push(newProduct);
+
+    res.status(201).json({
+        status: "success",
+        data: newProduct
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+});
