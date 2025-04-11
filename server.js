6875cfc5b11568614292ca56636c34325ed26b03
GuyDimension <billycelino67@gmail.com>
2025-04-11 08:35:54 +0700

    P5

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require ("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 let products = [
@@ -39,6 +40,52 @@ app.post("/products", (req, res) => {
     res.status(201).json({
         status: "success",
         data: newProduct
+    });
+});
+
+app.put("/products/:id", (req,res) => {
+    const id = parseInt(req.params.id);
+    const { name, price } = req.body;
+
+    const productIndex = products.findIndex(p => p.id === id);
+
+    if (productIndex === -1) {
+        return res.status(404).json({ status: "error", message: "Produk tidak ditemukan" });
+    }
+    if (!name || price === undefined || price === "") {
+        return res.status(400).json({
+            status: "error",
+            message: "Field 'name' dan 'price' wajib diisi"
+        });
+    }
+
+    products[productIndex] = {
+        id,
+        name,
+        price: parseFloat(price)
+    };
+
+    res.json({
+        status: "success",
+        message: "Produk berhasil diperbarui",
+        data: products[productIndex]
+    });
+});
+
+app.delete("/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const productIndex = products.findIndex(p => p.id === id);
+
+    if (productIndex === -1) {
+        return res.status(404).json({ status: "error", message: "Produk tidak ditemukan" });
+    }
+
+    const deletedProduct = products.splice(productIndex, 1);
+
+    res.json({
+        status: "success",
+        message: "Produk berhasil dihapus",
+        data: deletedProduct[0]
     });
 });
 
